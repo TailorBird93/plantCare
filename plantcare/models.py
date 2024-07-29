eb6545423d082d9b5ec8b72f9f6259d2e3cc53f3
Bartosz Krawczyk <bartoszkrawczyk93@gmail.com>
2024-07-29 18:16:22 +0000

    Fix issues with SQLAlchemy naming conventions when using db.create_all() command

--- plantcare/models.py
+++ plantcare/models.py
@@ -3,8 +3,10 @@ from plantcare import db
 
 
 class PlantCategory(db.Model):
+    
     id = db.Column(db.Integer, primary_key=True)
     plant_category_name = db.Column(db.String(25),unique=True, nullable=False)
+    plants = db.relationship("Plant", backref="plantcategory", cascade="all,delete", lazy=True)
     def __repr__(self):
         return self.plant_category_name
 
@@ -16,7 +18,7 @@ class Plant(db.Model):
     watering = db.Column(db.String(50), nullable=False)
     environment = db.Column(db.String(20), nullable=False)
     care_level = db.Column(db.String(20), nullable=False)
-    plant_category_id = db.Column(db.Integer, db.ForeignKey("plantcategory.id"), nullable=False)
+    plant_category_id = db.Column(db.Integer, db.ForeignKey("plant_category.id", ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
-        return self.name
+        return f"#{self.id} - Plant: {self.name} | Environment: {self.environment} | Watering: {self.watering} | Care Level: {self.care_level}"
--- plantcare/routes.py
+++ plantcare/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from plantcare import app, db
-
+from plantcare.models import PlantCategory, Plant
 
 @app.route("/")
 def home():
