b1990d7b21a8c7cd5265391808686e6309c5d1da
Bartosz Krawczyk <bartoszkrawczyk93@gmail.com>
2024-07-29 19:51:59 +0000

    Add JS initialisation for Materialise CSS

---
+++ plantcare/static/js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let sidenav = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
